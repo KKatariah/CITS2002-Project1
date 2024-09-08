0b4d51c2295476503bda7b0d1923e613610d32ca
Kat <106138621+KKatariah@users.noreply.github.com>
2024-09-08 19:28:42 +0800

    dunno

--- runml.c
+++ runml.c
@@ -1,7 +1,18 @@
-// Using online IDE for C 
 #include <stdio.h> 
+#include <stdlib.h> 
 
-int main() { 
-	printf("Hello World :3"); 
-	return 0; 
-} 
+int main(int argc, char *argv[]) { 
+	char file[] = read_file();
+}
+
+int readfile(char filename[])
+{
+    char file[] = fopen(filename, "r");
+
+    if(file == NULL) {
+        return 1;
+    }
+    
+    fclose(filename);
+    return 0;
+}
