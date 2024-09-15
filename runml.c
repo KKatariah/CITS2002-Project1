1c9898408c87e446982d413a38eaf969334d7bd1
Kat <106138621+KKatariah@users.noreply.github.com>
2024-09-15 16:07:19 +0800

    trying to work on tokeniser

--- runml.c
+++ runml.c
@@ -5,6 +5,7 @@
 
 #include <stdio.h> 
 #include <stdlib.h> 
+#include <string.h>
 
 int main(int argc, char *argv[]) { 
 	char file[] = read_file();
@@ -20,4 +21,42 @@ int readfile(char filename[])
     
     fclose(filename);
     return 0;
+}
+
+// list keywords 
+//const char keywords[] = {
+ //   "return",
+ //   "print",
+
+//}
+
+//Check syntax (idk what to call this)
+int is_thing(char ch) {
+    return (ch == '(' || ch == ')' || ch == ';')
+}
+int is_operator(char ch) {
+    return (ch == '+' || ch == '-' || ch == '*' || ch == '/' || ch == '%' || ch == '=')
+}
+int is_keyword (const char word) {
+    return (word == "return" || word == "print" || word == "function")
+}
+
+// Split each line into words
+int split_line(char line[], int word_count) {
+    int i = 0; //index of line
+    int j = 0; //index of current_word
+    char current_word[12]; //max length is 12 for words
+
+    while (line[i] != '\0') {
+        if (isspace(line[i])){
+            i++;
+            continue;
+        }
+        if isalpha(line[i]) {
+            while (isalpha(line[i]) && j <= 12) { //need to make sure an error is thrown if the word is too long
+                current_word[j] = line[i];
+                i++;
+            }
+        }
+    }
 }
